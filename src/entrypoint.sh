895a25eb0d9b72d37e51269d42b91113a8dc6938
afreefish666 <e2428887263@outlook.com>
2026-06-06 21:50:10 +0800

    Add files via upload

---
+++ src/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then 
+echo "No output path specified"
+exit 1;
+fi
+
+
+PROJECT_ID="$INPUT_SHARELATEX_PROJECT_ID"
+ZIP_OUTPUT_PATH="${1}/main.zip"
+EXTRACTED_FILES_PATH="./artifacts/"
+COOKIE=$(echo "$INPUT_SHARELATEX_COOKIE" | sed 's/.*=//g')
+HOST="$INPUT_SHARELATEX_HOST"
+
+echo "Dumping zip file at $ZIP_OUTPUT_PATH"
+
+curl "https://$HOST/project/$PROJECT_ID/download/zip" \
+  -H "authority: $HOST" \
+  -H 'pragma: no-cache' \
+  -H 'cache-control: no-cache' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'accept-language: en-US,en;q=0.9' \
+  -H "Cookie: overleaf.sid=$COOKIE" \
+  --output "$ZIP_OUTPUT_PATH" --create-dirs
+
+echo "Extracting all files at $EXTRACTED_FILES_PATH"
+
+unzip -o "$ZIP_OUTPUT_PATH" -d "$EXTRACTED_FILES_PATH"
+
